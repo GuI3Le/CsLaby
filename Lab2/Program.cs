f880737ea4dcef369cfe896e800841545232c670
Guble <kamilborucinski@gmail.com>
2024-10-11 17:58:08 +0200

    Zadanie lab 2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,19 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab2
 {
     internal abstract class Car
     {
-        protected bool IsStarted;
+        public bool IsStarted = false;
+        public bool IsFueled = false;
         public abstract void Start();
         public abstract void Stop();
         public abstract void Fuel();
         public abstract void Drive();
+
+        public void TestDrive(Car car)
+        {
+            car.Fuel();
+            car.Fuel();
+            car.Drive();
+            car.Start();
+            car.Start();
+            car.Drive();
+            car.Drive();
+            car.Stop();
+            car.Stop();
+        }
     }
 
     internal class PetrolCar : Car
     {
-        private bool _isFueled;
 
         public override void Start()
         {
@@ -43,10 +57,10 @@ namespace Lab2
 
         public override void Fuel()
         {
-            if (!_isFueled)
+            if (!IsFueled)
             {
                 Console.WriteLine("Fueling up car with petrol");
-                _isFueled = true;
+                IsFueled = true;
             }
             else
             {
@@ -58,9 +72,10 @@ namespace Lab2
         {
             if (IsStarted)
             {
-                if (_isFueled)
+                if (IsFueled)
                 {
                     Console.WriteLine("Driving on petrol");
+                    IsFueled = false;
                 }
                 else
                 {
@@ -72,22 +87,153 @@ namespace Lab2
                 Console.WriteLine("Car is not started");
             }
         }
+    }
 
-        // class GasCar : Car
-        // {
-        //     
-        // }
-        //
-        // class ElectricCar : Car
-        // {
-        //     
-        // }
+    internal class GasCar : Car
+    {
+
+        public override void Start()
+        {
+            if (!IsStarted)
+            {
+                Console.WriteLine("Starting car");
+                IsStarted = true;
+            }
+            else
+            {
+                Console.WriteLine("Car is already started");
+            }
+        }
+
+        public override void Stop()
+        {
+            if (IsStarted)
+            {
+                Console.WriteLine("Stopping car");
+                IsStarted = false;
+            }
+            else
+            {
+                Console.WriteLine("Car is not started");
+            }
+        }
+        public override void Fuel()
+        {
+            if (!IsFueled)
+            {
+                Console.WriteLine("Fueling up car with gas");
+                IsFueled = true;
+            }
+            else
+            {
+                Console.WriteLine("Car is already fueled up with gas");
+            }
+        }
+
+        public override void Drive()
+        {
+            if (IsStarted && IsFueled)
+            {
+                Console.WriteLine("Driving on gas");
+                IsFueled= false;
+            }
+            else if (!IsFueled) {
+                Console.WriteLine("Not enough gas, fuel up");
+
+            }
+            else
+            {
+                Console.WriteLine("Car is not started");
+
+            }
+        }
+
+    }
+
+    internal class ElectricCar : Car
+    {
+        public override void Start()
+        {
+            if (!IsStarted)
+            {
+                Console.WriteLine("Starting car");
+                IsStarted = true;
+            }
+            else
+            {
+                Console.WriteLine("Car is already started");
+            }
+        }
+
+        public override void Stop()
+        {
+            if (IsStarted)
+            {
+                Console.WriteLine("Stopping car");
+                IsStarted = false;
+            }
+            else
+            {
+                Console.WriteLine("Car is not started");
+            }
+        }
+
+        public override void Fuel()
+        {
+            if (!IsFueled)
+            {
+                Console.WriteLine("Charging car");
+                IsFueled = true;
+            }
+            else
+            {
+                Console.WriteLine("Car is already charged");
+            }
+        }
+
+        public override void Drive()
+        {
+            if (IsStarted)
+            {
+                if (IsFueled)
+                {
+                    Console.WriteLine("Driving on electricity");
+                    IsFueled = false;
+                }
+                else
+                {
+                    Console.WriteLine("Not enough electricity, charge the car");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Car is not started");
+            }
+        }
+    }
+
+    
 
         internal class Program
         {
             public static void Main(string[] args)
             {
+            ElectricCar electricCar = new ElectricCar();
+            PetrolCar petrolCar = new PetrolCar();
+            GasCar gasCar = new GasCar();
+            var carList = new List<Car>();
+            carList.Add(petrolCar);
+            carList.Add(gasCar);
+            carList.Add(electricCar);
+            foreach (var car in carList)
+            {
+                car.TestDrive(car);
+                Console.WriteLine();
             }
+
+
+
         }
-    }
+        }
+    
 }
