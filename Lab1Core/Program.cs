813ebce6e4bf3e98f421ade8dfe381c9c6d131a2
GuI3Le <60442125+GuI3Le@users.noreply.github.com>
2024-11-10 19:18:22 +0100

    utworzenie projektu o funkcjonalności lab 1 w .net core

---
+++ Lab1Core/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lab1Core;
+
+CsvToHtml.Convert("C:\\Users\\kamil\\source\\repos\\CsLaby\\Lab1Core\\test_file.csv", "C:\\Users\\kamil\\source\\repos\\CsLaby\\Lab1Core\\html_test.html");
+
